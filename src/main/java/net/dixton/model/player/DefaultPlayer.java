06b25035d875585f7cae85b3e7e280f20a11881d
GuilhermeDSantos <53098377+GuilhermeDSantos@users.noreply.github.com>
2024-11-12 14:35:46 -0300

    Created Player models

---
+++ src/main/java/net/dixton/model/player/DefaultPlayer.java
@@ -0,0 +1,15 @@
+package net.dixton.model.player;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import net.dixton.model.account.Account;
+
+@Getter
+@Setter
+@ToString
+public class DefaultPlayer {
+
+    private Account account;
+
+}
