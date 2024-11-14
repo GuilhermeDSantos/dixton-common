27cd97467a9e9eba4d28e7c820a2fdb4ad160a51
GuilhermeDSantos <53098377+GuilhermeDSantos@users.noreply.github.com>
2024-11-14 20:26:11 -0300

    Added server type, status and memory settings to the server model

--- src/main/java/net/dixton/model/server/Server.java
+++ src/main/java/net/dixton/model/server/Server.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import net.dixton.enums.ServerStatus;
+import net.dixton.enums.ServerType;
 import net.dixton.model.Role;
 
 @Entity
@@ -21,6 +23,9 @@ public class Server {
 
     private Integer port, maxPlayers;
 
+    @Column(nullable = false)
+    private Integer minimumMemory, maximumMemory;
+
     @Column(columnDefinition = "TINYINT(1)")
     private boolean whitelisted, autoStart;
 
@@ -31,4 +36,10 @@ public class Server {
     @ManyToOne
     @JoinColumn(name = "needed_role_id")
     private Role neededRole;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ServerType type;
+
+    private ServerStatus status;
 }
